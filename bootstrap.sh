3a6484b8b4a4548323897f0ee97c585bd602c27a
Aurélien Praga <aurelien.praga@inovia-team.com>
2015-09-02 19:00:15 +0200

    Config Vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 # Use single quotes instead of double quotes to make it work with special-character passwords
 PASSWORD='test'
 PROJECTFOLDER='nutrition'
+DATABASE='nutrition'
 
 # create project folder
 if [ ! -d "/var/www/${PROJECTFOLDER}" ]; then
@@ -32,6 +33,10 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get -y install phpmyadmin
 
+# import database
+mysql -u root -p${PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${DATABASE};"
+mysql -u root -p${PASSWORD} ${DATABASE} < /var/www/${PROJECTFOLDER}/ws/db/db.sql
+
 # setup hosts file
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
@@ -53,3 +58,30 @@ sudo apt-get -y install git vim curl
 
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+
+# configure ws
+sudo chmod 777 "/var/www/${PROJECTFOLDER}/ws/logs"
+
+DATABASECONFIG=$(cat <<EOF
+<?php
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+	'monolog.logfile' => __DIR__.'/../../logs/dev.log',
+	'monolog.name'    => 'nutrition'
+));
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+	'db.options' => array(
+		'driver'   => 'pdo_mysql',
+		'dbname'   => 'nutrition',
+		'host'     => 'localhost',
+		'user'     => 'root',
+		'password' => '',
+		'charset'  => 'utf8'
+	)
+));
+EOF
+)
+echo "${DATABASECONFIG}" > /var/www/${PROJECTFOLDER}/ws/app/config/prod.php
